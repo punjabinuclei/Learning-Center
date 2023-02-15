16acb3c0e7b31223672f72f1c12bbf585e814307
punjabinuclei <guppi0204@gmail.com>
2023-02-15 15:30:31 +0530

    new sections added

---
+++ index.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+$('.owl-1').owlCarousel({
+    loop: true,
+    margin: 10,
+    // autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
+
+$('.owl-2').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 1,
+            nav: false
+        },
+        1000: {
+            items: 1,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
+$('.owl-3').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
