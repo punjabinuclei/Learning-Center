685fe8ba602ed67d32a4994894bc709dd298dc10
punjabinuclei <guppi0204@gmail.com>
2023-02-16 11:19:54 +0530

    MinorFixes

--- index.js
+++ index.js
@@ -74,3 +74,23 @@ $('.owl-3').owlCarousel({
 })
 
 
+
+
+
+ScrollReveal().reveal('#stats', { delay: 1000 });
+ScrollReveal().reveal('#sucessStory', { delay: 1000 });
+ScrollReveal().reveal('#best1', { delay: 1000 });
+ScrollReveal().reveal('#best2', { delay: 1000 });
+ScrollReveal().reveal('#best3', { delay: 1000 });
+ScrollReveal().reveal('#founder1', { delay: 1000 });
+ScrollReveal().reveal('#founder2', { delay: 1000 });
+ScrollReveal().reveal('#vision', { delay: 1000 });
+ScrollReveal().reveal('#faculty1', { delay: 1000 });
+ScrollReveal().reveal('#faculty2', { delay: 1000 });
+ScrollReveal().reveal('#achieve1', { delay: 1000 });
+ScrollReveal().reveal('#achieve2', { delay: 1000 });
+ScrollReveal().reveal('#testimonials1', { delay: 1000 });
+ScrollReveal().reveal('#testimonials2', { delay: 1000 });
+ScrollReveal().reveal('#gallery1', { delay: 1000 });
+ScrollReveal().reveal('#gallery2', { delay: 1000 });
+ScrollReveal().reveal('#footer', { delay: 1000 });
